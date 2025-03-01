684ecdf89a47273adbe0fc43f7a36d66e9841e88
panyawat <panyawatkrutnak@gmail.com>
2025-03-02 01:13:05 +0700

    ตะกร้า
    และรูปตะกร้าหน้าหลัก

--- navbar.php
+++ navbar.php
@@ -1,9 +1,4 @@
-<?php require_once('condb.php');
-$query_typeprd = "SELECT * FROM tbl_type ORDER BY type_id ASC";
-$typeprd = mysqli_query($con, $query_typeprd) or die("Error in query: $query_typeprd " . mysqli_error($con));
-$row_typeprd = mysqli_fetch_assoc($typeprd);
-$totalRows_typeprd = mysqli_num_rows($typeprd);
-?>
+<?php require_once('condb.php'); $query_typeprd = "SELECT * FROM tbl_type ORDER BY type_id ASC"; $typeprd = mysqli_query($con, $query_typeprd) or die("Error in query: $query_typeprd " . mysqli_error($con)); $row_typeprd = mysqli_fetch_assoc($typeprd); $totalRows_typeprd = mysqli_num_rows($typeprd); ?> 
 <div class="container-fluid">
     <div class="row">
         <div class="col-md-12">
@@ -32,7 +27,11 @@ $totalRows_typeprd = mysqli_num_rows($typeprd);
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
                     </ul>
-                    <form class="form-inline my-2 my-lg-0 ml-3" name="qp" action="index.php" method="GET">
+                    <!-- Cart Image Icon (No Background) -->
+                    <a href="cart.php" class="mr-2" title="ตะกร้าสินค้า" style="display: inline-block; margin-top: 5px;">
+                        <img src="m_img/8648831.png" alt="ตะกร้าสินค้า" style="height: 25px; width: auto;">
+                    </a>
+                    <form class="form-inline my-2 my-lg-0" name="qp" action="index.php" method="GET">
                         <input class="form-control mr-sm-2" type="text" placeholder="ค้นหาสินค้า" name="q">
                         <button class="btn btn-secondary my-2 my-sm-0" type="submit">ค้นหา</button>
                     </form>
